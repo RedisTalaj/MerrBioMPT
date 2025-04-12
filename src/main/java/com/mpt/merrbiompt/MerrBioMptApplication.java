8d3115327f315d903c6a47ea5b8a722c22a145a1
RedisTalaj <102618563+RedisTalaj@users.noreply.github.com>
2025-04-12 15:29:28 +0200

    first commit

---
+++ src/main/java/com/mpt/merrbiompt/MerrBioMptApplication.java
@@ -0,0 +1,13 @@
+package com.mpt.merrbiompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerrBioMptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerrBioMptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mpt/merrbiompt/MerrBioMptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpt.merrbiompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerrBioMptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
